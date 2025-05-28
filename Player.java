57d6d014ef36ff0d00a3636a65cf2d5e85581866
clamentsai <syaichulmubaraq04@gmail.com>
2025-05-28 12:11:47 +0700

    refactor: initiate gold to 500

--- Player.java
+++ Player.java
@@ -17,7 +17,7 @@ public class Player {
         this.energy = MAX_ENERGY; 
         this.farmName = farmName;
         this.partner = null; 
-        this.gold = 0; 
+        this.gold = 500; // Initial gold amount
         this.inventory = new Inventory();
         this.location = "Farm"; 
     }
@@ -62,7 +62,7 @@ public class Player {
         this.farmName = farmName;
     }
     
-    public String partner() {
+    public String getPartner() {
         return partner;
     }
 
@@ -92,5 +92,10 @@ public class Player {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Player: " + name + ", Gold: " + gold + "g, Energy: " + energy;
     }
 }
