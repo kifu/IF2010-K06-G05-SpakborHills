6fbfed75e5568214ebae8938ab61a8a3d15a59c7
clamentsai <syaichulmubaraq04@gmail.com>
2025-05-28 10:58:11 +0700

    refactor: add toString to Misc and Equipment

--- Equipment.java
+++ Equipment.java
@@ -2,4 +2,13 @@ public class Equipment extends Item {
     public Equipment(String name) {
         super(name, 0, 0);
     }
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "name='" + getName() + '\'' +
+                ", buyPrice=" + getBuyPrice() +
+                ", sellPrice=" + getSellPrice() +
+                '}';
+    }
 }
--- Misc.java
+++ Misc.java
@@ -2,4 +2,13 @@ public class Misc extends Item {
     public Misc(String name, int buyPrice, int sellPrice) {
         super(name, buyPrice, sellPrice);
     }
+
+    @Override
+    public String toString() {
+        return "Misc{" +
+                "name='" + getName() + '\'' +
+                ", buyPrice=" + getBuyPrice() +
+                ", sellPrice=" + getSellPrice() +
+                '}';
+    }
 }
